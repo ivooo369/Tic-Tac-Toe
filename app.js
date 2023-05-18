8ae05876d75ce2fca6e8c4a7bad18991fe0ae535
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-05-19 00:45:47 +0300

    Add animation to some elements on the starting page

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+const playerContainers = document.querySelectorAll('.player-containers');
+
+let selectedSigns = [];
+
+playerContainers.forEach((container, index) => {
+    const signs = container.querySelectorAll('.signs');
+    signs.forEach(sign => {
+        sign.addEventListener('click', function () {
+            const isSelected = this.classList.contains('active');
+            if (selectedSigns[index]) {
+                selectedSigns[index].classList.remove('active');
+            }
+            if (!isSelected) {
+                this.classList.add('active');
+                selectedSigns[index] = this;
+            } else {
+                selectedSigns[index] = null;
+            }
+        });
+    });
+});
+
 const Gameboard = {
     gameboardArray: []
 };
