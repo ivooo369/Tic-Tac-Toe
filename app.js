34b65d27ca0e7a0afff1603af3deec7cea9ede47
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-05-27 23:20:37 +0300

    Start applying game logic

--- app.js
+++ app.js
@@ -136,6 +136,44 @@ function validateSignSelection() {
     return true;
 }
 
-const Gameboard = {
-    gameboardArray: []
-};
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+    const getBoard = () => board;
+
+    const putSign = (row, player) => {
+        const availableCells = board.filter((column) => column[row].getValue() === 0).map(column => column[row]);
+    };
+
+    if (!availableCells.length) return;
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, putSign, printBoard };
+}
+
+function Cell() {
+    let value = 0;
+
+    const putSign = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        putSign,
+        getValue
+    };
+}
