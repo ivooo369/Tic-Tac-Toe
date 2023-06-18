4111ac65707fb1a9466f62e5b5b5edb68ba0941d
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-06-18 23:12:11 +0300

    Reorganize the code and make the score counters work

--- app.js
+++ app.js
@@ -1,178 +1,178 @@
-const playerOne_X = document.querySelector('#playerOne-X');
-const playerOne_O = document.querySelector('#playerOne-O');
-const playerTwo_X = document.querySelector('#playerTwo-X');
-const playerTwo_O = document.querySelector('#playerTwo-O');
-const startButton = document.querySelector('#start-button');
-const startPageContainer = document.querySelector('.start-page-container');
-const mainPageContainer = document.querySelector('.main-page-container');
-const contestantsNames = document.querySelector('#contestants-names');
-const inputPlayerOneName = document.querySelector('#input-playerOne-name');
-const inputPlayerTwoName = document.querySelector('#input-playerTwo-name');
-const spanPlayerOneName = document.querySelector('#span-playerOne-name');
-const spanPlayerTwoName = document.querySelector('#span-playerTwo-name');
-const signs = document.querySelectorAll('.signs');
-const playerOneScore = document.querySelector('#playerOne-score');
-const numberOfTies = document.querySelector('#number-of-ties');
-const playerTwoScore = document.querySelector('#playerTwo-score');
-const exit = document.querySelector('#exit');
+function initializeGame() {
+    const playerOne_X = document.querySelector('#playerOne-X');
+    const playerOne_O = document.querySelector('#playerOne-O');
+    const playerTwo_X = document.querySelector('#playerTwo-X');
+    const playerTwo_O = document.querySelector('#playerTwo-O');
+    const startButton = document.querySelector('#start-button');
+    const startPageContainer = document.querySelector('.start-page-container');
+    const mainPageContainer = document.querySelector('.main-page-container');
+    const inputPlayerOneName = document.querySelector('#input-playerOne-name');
+    const inputPlayerTwoName = document.querySelector('#input-playerTwo-name');
+    const signs = document.querySelectorAll('.signs');
+    const playerOneScore = document.querySelector('#playerOne-score');
+    const numberOfTies = document.querySelector('#number-of-ties');
+    const playerTwoScore = document.querySelector('#playerTwo-score');
+    const exit = document.querySelector('#exit');
+    const counterTotalRounds = document.querySelector('#counter-total-rounds');
+    const counterPlayerOneScore = document.querySelector('#counter-playerOne-score');
+    const counterPlayerTwoScore = document.querySelector('#counter-playerTwo-score');
+    const counterNumberOfTies = document.querySelector('#counter-number-of-ties');
 
-let isPlayerOne_X = false;
-let isPlayerOne_O = false;
-let isPlayerTwo_X = false;
-let isPlayerTwo_O = false;
+    let isPlayerOne_X = false;
+    let isPlayerOne_O = false;
+    let isPlayerTwo_X = false;
+    let isPlayerTwo_O = false;
 
-let playerOneChoice = '';
-let playerTwoChoice = '';
+    let playerOneChoice = '';
+    let playerTwoChoice = '';
 
-playerOne_X.addEventListener('click', function () {
-    isPlayerOne_X = true;
-    isPlayerOne_O = false;
-    isPlayerTwo_X = false;
-    isPlayerTwo_O = false;
-    selectSign();
-});
+    playerOne_X.addEventListener('click', function () {
+        isPlayerOne_X = true;
+        isPlayerOne_O = false;
+        isPlayerTwo_X = false;
+        isPlayerTwo_O = false;
+        putSign();
+    });
 
-playerOne_O.addEventListener('click', function () {
-    isPlayerOne_O = true;
-    isPlayerOne_X = false;
-    isPlayerTwo_X = false;
-    isPlayerTwo_O = false;
-    selectSign();
-});
+    playerOne_O.addEventListener('click', function () {
+        isPlayerOne_O = true;
+        isPlayerOne_X = false;
+        isPlayerTwo_X = false;
+        isPlayerTwo_O = false;
+        putSign();
+    });
 
-playerTwo_X.addEventListener('click', function () {
-    isPlayerTwo_X = true;
-    isPlayerOne_X = false;
-    isPlayerOne_O = false;
-    isPlayerTwo_O = false;
-    selectSign();
-});
+    playerTwo_X.addEventListener('click', function () {
+        isPlayerTwo_X = true;
+        isPlayerOne_X = false;
+        isPlayerOne_O = false;
+        isPlayerTwo_O = false;
+        putSign();
+    });
 
-playerTwo_O.addEventListener('click', function () {
-    isPlayerTwo_O = true;
-    isPlayerOne_X = false;
-    isPlayerOne_O = false;
-    isPlayerTwo_X = false;
-    selectSign();
-});
+    playerTwo_O.addEventListener('click', function () {
+        isPlayerTwo_O = true;
+        isPlayerOne_X = false;
+        isPlayerOne_O = false;
+        isPlayerTwo_X = false;
+        putSign();
+    });
 
-startButton.addEventListener('click', function (e) {
-    if (validateNames() && validateSignSelection()) {
-        e.preventDefault();
-        startPageContainer.style.display = 'none';
-        mainPageContainer.style.display = 'flex';
-        spanPlayerOneName.innerText = `${inputPlayerOneName.value}`;
-        spanPlayerTwoName.innerText = `${inputPlayerTwoName.value}`;
-        playerOneScore.innerText = inputPlayerOneName.value;
-        numberOfTies.innerText = 'Ties';
-        playerTwoScore.innerText = inputPlayerTwoName.value;
+    startButton.addEventListener('click', function (e) {
+        const spanPlayerOneName = document.querySelector('#span-playerOne-name');
+        const spanPlayerTwoName = document.querySelector('#span-playerTwo-name');
 
-        // Start the game controller
-        const game = GameController(inputPlayerOneName.value, inputPlayerTwoName.value, playerOneChoice, playerTwoChoice);
-        const boardDiv = document.querySelector("#game-board");
+        if (validateNames() && validateSignSelection()) {
+            e.preventDefault();
+            startPageContainer.style.display = 'none';
+            mainPageContainer.style.display = 'flex';
+            spanPlayerOneName.innerText = `${inputPlayerOneName.value}`;
+            spanPlayerTwoName.innerText = `${inputPlayerTwoName.value}`;
+            playerOneScore.innerText = inputPlayerOneName.value;
+            numberOfTies.innerText = 'Ties';
+            playerTwoScore.innerText = inputPlayerTwoName.value;
+            const game = GameController(inputPlayerOneName.value, inputPlayerTwoName.value, playerOneChoice, playerTwoChoice);
+            const boardDiv = document.querySelector("#game-board");
 
-        const updateScreen = () => {
-            // Clear the board
-            boardDiv.innerHTML = "";
-
-            // Get the newest version of the board and active player
-            const board = game.getBoard();
-
-            // Render board squares
-            board.forEach((row, rowIndex) => {
-                row.forEach((cell, columnIndex) => {
-                    const cellButton = document.createElement("button");
-                    cellButton.classList.add("cell");
-                    cellButton.textContent = cell.getValue();
-                    cellButton.addEventListener("click", () => clickHandlerBoard(rowIndex, columnIndex));
-                    boardDiv.appendChild(cellButton);
+            const updateScreen = () => {
+                boardDiv.innerHTML = "";
+                const board = game.getBoard();
+                board.forEach((row, rowIndex) => {
+                    row.forEach((cell, columnIndex) => {
+                        const cellButton = document.createElement("button");
+                        cellButton.classList.add("cell");
+                        cellButton.textContent = cell.getValue();
+                        cellButton.addEventListener("click", () => clickHandlerBoard(rowIndex, columnIndex));
+                        boardDiv.appendChild(cellButton);
+                    });
                 });
-            });
-        };
+            };
 
-        // Add event listener for the board
-        function clickHandlerBoard(row, column) {
-            game.playRound(row, column);
+            function clickHandlerBoard(row, column) {
+                game.playRound(row, column);
+                updateScreen();
+            }
             updateScreen();
         }
-
-        // Initial render
-        updateScreen();
-    }
-});
-
-inputPlayerOneName.addEventListener('input', () => inputPlayerOneName.setCustomValidity(''));
-inputPlayerTwoName.addEventListener('input', () => inputPlayerTwoName.setCustomValidity(''));
-
-exit.addEventListener('click', function () {
-    mainPageContainer.style.display = 'none';
-    startPageContainer.style.display = 'flex';
-    inputPlayerOneName.value = '';
-    inputPlayerTwoName.value = '';
-    signs.forEach(sign => {
-        sign.classList.remove('active');
     });
-});
 
-function selectSign() {
-    if (isPlayerOne_X) {
-        playerOneChoice = 'X';
-        playerOne_X.classList.toggle('active');
-        playerOne_O.classList.remove('active');
-        playerTwo_X.classList.remove('active');
-    } else if (isPlayerOne_O) {
-        playerOneChoice = 'O';
-        playerOne_O.classList.toggle('active');
-        playerOne_X.classList.remove('active');
-        playerTwo_O.classList.remove('active');
-    } else if (isPlayerTwo_X) {
-        playerTwoChoice = 'X';
-        playerTwo_X.classList.toggle('active');
-        playerOne_X.classList.remove('active');
-        playerTwo_O.classList.remove('active');
-    } else if (isPlayerTwo_O) {
-        playerTwoChoice = 'O';
-        playerTwo_O.classList.toggle('active');
-        playerOne_O.classList.remove('active');
-        playerTwo_X.classList.remove('active');
-    }
-}
+    inputPlayerOneName.addEventListener('input', () => inputPlayerOneName.setCustomValidity(''));
+    inputPlayerTwoName.addEventListener('input', () => inputPlayerTwoName.setCustomValidity(''));
 
-function validateNames() {
-    if (inputPlayerOneName.value === '') {
-        inputPlayerOneName.setCustomValidity('Please enter a name for Player 1.');
-        inputPlayerOneName.reportValidity();
-        return false;
-    } else if (inputPlayerTwoName.value === '') {
-        inputPlayerTwoName.setCustomValidity('Please enter a name for Player 2.');
-        inputPlayerTwoName.reportValidity();
-        return false;
-    } else if (inputPlayerOneName.value === inputPlayerTwoName.value) {
-        inputPlayerTwoName.setCustomValidity('Please enter different names for both players.');
-        inputPlayerTwoName.reportValidity();
-        return false;
-    }
-    if (!(inputPlayerOneName.value.length >= 3 && inputPlayerOneName.value.length <= 15)) {
-        inputPlayerOneName.setCustomValidity('The name of the player must be between 3 and 15 characters.');
-        inputPlayerOneName.reportValidity();
-        return false;
-    } else if (!(inputPlayerTwoName.value.length >= 3 && inputPlayerTwoName.value.length <= 15)) {
-        inputPlayerTwoName.setCustomValidity('The name of the player must be between 3 and 15 characters.');
-        inputPlayerTwoName.reportValidity();
-        return false;
-    }
-    return true;
-}
-
-function validateSignSelection() {
-    if (!(playerOne_X.classList.contains('active') || playerOne_O.classList.contains('active')) ||
-        !(playerTwo_X.classList.contains('active') || playerTwo_O.classList.contains('active'))) {
+    exit.addEventListener('click', function () {
+        mainPageContainer.style.display = 'none';
+        startPageContainer.style.display = 'flex';
+        inputPlayerOneName.value = '';
+        inputPlayerTwoName.value = '';
+        counterTotalRounds.innerText = 0;
+        counterPlayerOneScore.innerText = 0;
+        counterPlayerTwoScore.innerText = 0;
+        counterNumberOfTies.innerText = 0;
         signs.forEach(sign => {
-            sign.style.border = '3px solid red';
+            sign.classList.remove('active');
         });
-        return false;
+    });
+
+    function putSign() {
+        if (isPlayerOne_X) {
+            playerOneChoice = 'X';
+            playerOne_X.classList.toggle('active');
+            playerOne_O.classList.remove('active');
+            playerTwo_X.classList.remove('active');
+        } else if (isPlayerOne_O) {
+            playerOneChoice = 'O';
+            playerOne_O.classList.toggle('active');
+            playerOne_X.classList.remove('active');
+            playerTwo_O.classList.remove('active');
+        } else if (isPlayerTwo_X) {
+            playerTwoChoice = 'X';
+            playerTwo_X.classList.toggle('active');
+            playerOne_X.classList.remove('active');
+            playerTwo_O.classList.remove('active');
+        } else if (isPlayerTwo_O) {
+            playerTwoChoice = 'O';
+            playerTwo_O.classList.toggle('active');
+            playerOne_O.classList.remove('active');
+            playerTwo_X.classList.remove('active');
+        }
     }
-    return true;
+
+    function validateNames() {
+        if (inputPlayerOneName.value === '') {
+            inputPlayerOneName.setCustomValidity('Please enter a name for Player 1.');
+            inputPlayerOneName.reportValidity();
+            return false;
+        } else if (inputPlayerTwoName.value === '') {
+            inputPlayerTwoName.setCustomValidity('Please enter a name for Player 2.');
+            inputPlayerTwoName.reportValidity();
+            return false;
+        } else if (inputPlayerOneName.value === inputPlayerTwoName.value) {
+            inputPlayerTwoName.setCustomValidity('Please enter different names for both players.');
+            inputPlayerTwoName.reportValidity();
+            return false;
+        }
+        if (!(inputPlayerOneName.value.length >= 3 && inputPlayerOneName.value.length <= 15)) {
+            inputPlayerOneName.setCustomValidity('The name of the player must be between 3 and 15 characters.');
+            inputPlayerOneName.reportValidity();
+            return false;
+        } else if (!(inputPlayerTwoName.value.length >= 3 && inputPlayerTwoName.value.length <= 15)) {
+            inputPlayerTwoName.setCustomValidity('The name of the player must be between 3 and 15 characters.');
+            inputPlayerTwoName.reportValidity();
+            return false;
+        }
+        return true;
+    }
+
+    function validateSignSelection() {
+        if (!(playerOne_X.classList.contains('active') || playerOne_O.classList.contains('active')) ||
+            !(playerTwo_X.classList.contains('active') || playerTwo_O.classList.contains('active'))) {
+            signs.forEach(sign => {
+                sign.style.border = '3px solid red';
+            });
+            return false;
+        }
+        return true;
+    }
 }
 
 function Gameboard() {
@@ -199,7 +199,6 @@ function Gameboard() {
     };
 
     const checkWinner = (board) => {
-        const counterTotalRounds = document.querySelector('#counter-total-rounds');
         const winningCombinations = [
             // Rows
             [[0, 0], [0, 1], [0, 2]],
@@ -226,10 +225,10 @@ function Gameboard() {
                 board[rowA][colA].getValue() === board[rowB][colB].getValue() &&
                 board[rowA][colA].getValue() === board[rowC][colC].getValue()
             ) {
-                return true; // There is a winner
+                return true;
             }
         }
-        return false; // No winner
+        return false;
     };
 
     const printBoard = () => {
@@ -257,8 +256,7 @@ function Cell() {
     };
 }
 
-function GameController(playerOneName = inputPlayerOneName.value, playerTwoName = inputPlayerTwoName.value, playerOneChoice,
-    playerTwoChoice) {
+function GameController(playerOneName, playerTwoName, playerOneChoice, playerTwoChoice) {
     const board = Gameboard();
 
     const players = [
@@ -272,7 +270,6 @@ function GameController(playerOneName = inputPlayerOneName.value, playerTwoName 
         },
     ];
 
-    // Set the active player based on the chosen signs
     let activePlayer = players.find(player => player.sign === 'X') || players[0];
 
     const switchPlayerTurn = () => {
@@ -293,8 +290,12 @@ function GameController(playerOneName = inputPlayerOneName.value, playerTwoName 
 
     const playRound = (row, column) => {
         const counterTotalRounds = document.querySelector('#counter-total-rounds');
+        const counterPlayerOneScore = document.querySelector('#counter-playerOne-score');
+        const counterPlayerTwoScore = document.querySelector('#counter-playerTwo-score');
+        const counterNumberOfTies = document.querySelector('#counter-number-of-ties');
+
         if (checkWinner()) {
-            return; // Don't allow further moves if there is already a winner
+            return;
         }
         const signPlaced = board.placeSign(row, column, getActivePlayer().sign);
         if (signPlaced) {
@@ -302,12 +303,20 @@ function GameController(playerOneName = inputPlayerOneName.value, playerTwoName 
                 const playerTurnDiv = document.querySelector("#player-turn");
                 playerTurnDiv.textContent = `${activePlayer.name} wins!`;
                 counterTotalRounds.textContent = parseInt(counterTotalRounds.textContent + 1);
+                if (activePlayer.name === playerOneName) {
+                    counterPlayerOneScore.textContent = parseInt(counterPlayerOneScore.textContent + 1);
+                    return;
+                } else if (activePlayer.name === playerTwoName) {
+                    counterPlayerTwoScore.textContent = parseInt(counterPlayerTwoScore.textContent + 1);
+                    return;
+                }
                 return;
             }
             if (isTie()) {
                 const playerTurnDiv = document.querySelector("#player-turn");
                 playerTurnDiv.textContent = "It's a tie!";
                 counterTotalRounds.textContent = parseInt(counterTotalRounds.textContent + 1);
+                counterNumberOfTies.textContent = parseInt(counterNumberOfTies.textContent + 1);
                 return;
             }
             switchPlayerTurn();
@@ -330,32 +339,26 @@ function GameController(playerOneName = inputPlayerOneName.value, playerTwoName 
 }
 
 function ScreenController() {
+    initializeGame();
     const game = GameController();
     const boardDiv = document.querySelector("#game-board");
 
     const updateBoard = () => {
-        // Clear the board
         boardDiv.innerHTML = "";
-
-        // Get the newest version of the board and active player
         const board = game.getBoard();
-        // const activePlayer = game.getActivePlayer();
-
-        // Render board squares
         board.forEach((row, rowIndex) => {
             row.forEach((cell, columnIndex) => {
                 const cellButton = document.createElement("button");
                 cellButton.classList.add("cell");
                 cellButton.textContent = cell.getValue();
-                cellButton.dataset.row = rowIndex; // Set the row index as a dataset attribute
-                cellButton.dataset.column = columnIndex; // Set the column index as a dataset attribute
+                cellButton.dataset.row = rowIndex;
+                cellButton.dataset.column = columnIndex;
                 cellButton.addEventListener("click", clickHandlerBoard);
                 boardDiv.appendChild(cellButton);
             });
         });
     };
 
-    // Add event listener for the board
     function clickHandlerBoard(e) {
         const selectedButton = e.target;
         const row = parseInt(selectedButton.dataset.row);
@@ -366,10 +369,7 @@ function ScreenController() {
             updateBoard();
         }
     }
-
     boardDiv.addEventListener("click", clickHandlerBoard);
-
-    // Initial render
     updateBoard();
 }
 
